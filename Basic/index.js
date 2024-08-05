4891f37efb691f7d0b20def7a212cf9c8dcf2997
ashish9534 <kumarashish69942@gmail.com>
2024-08-05 15:57:40 +0530

    first commit

---
+++ Basic/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+const PORT = 5000;
+const app = express();
+
+app.get("./home", (req, res) => {
+  res.json("HELLO everyone");
+  res.send("welcome to chai and code ");
+});
+
+app.listen(PORT, () => {
+  console.log(`server is runing on port no ${PORT}`);
+});
